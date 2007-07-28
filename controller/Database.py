f81707ea2f252df723aaf635f55b6b5e557a976a
Dan Helfman <dan@luminotes.com>
2007-07-28 04:11:14 +0000

    For none_okay validator flag, allow empty strings as well.

--- controller/Database.py
+++ controller/Database.py
@@ -259,7 +259,7 @@ class Valid_id( object ):
     self.__none_okay = none_okay
 
   def __call__( self, value ):
-    if self.__none_okay and value is None: return None
+    if self.__none_okay and value in ( None, "" ): return None
     if self.ID_PATTERN.search( value ): return str( value )
 
     raise ValueError()
