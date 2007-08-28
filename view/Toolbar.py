8f6365c41ed0bb62630c20f0b72cee9647c8fe75
Dan Helfman <dan@luminotes.com>
2007-08-28 22:07:11 +0000

    Reordering the toolbar buttons a bit.

--- view/Toolbar.py
+++ view/Toolbar.py
@@ -6,14 +6,18 @@ class Toolbar( Div ):
     Div.__init__(
       self,
       Ul(
+        Li( Input( type = u"button", value = u"+", id = u"newNote", title = u"new note [ctrl-N]", class_ = u"button" ) ),
+        Li( Input( type = u"button", value = u"&#8594", id = u"createLink", title = u"note link [ctrl-L]", class_ = u"button" ) ),
+      ),
+      Ul(
         Li( Input( type = u"button", value = u"B", id = u"bold", title = u"bold [ctrl-B]", class_ = u"button" ) ),
         Li( Input( type = u"button", value = u"I", id = u"italic", title = u"italic [ctrl-I]", class_ = u"button" ) ),
         Li( Input( type = u"button", value = u"U", id = u"underline", title = u"underline [ctrl-U]", class_ = u"button" ) ),
         Li( Input( type = u"button", value = u"T", id = u"title", title = u"title [ctrl-T]", class_ = u"button" ) ),
+      ),
+      Ul(
         Li( Input( type = u"button", value = u"&#149;", id = u"insertUnorderedList", title = u"bullet list [ctrl-period]", class_ = u"button" ) ),
         Li( Input( type = u"button", value = u"1.", id = u"insertOrderedList", title = u"numbered list [ctrl-1]", class_ = u"button" ) ),
-        Li( Input( type = u"button", value = u"&#8594", id = u"createLink", title = u"note link [ctrl-L]", class_ = u"button" ) ),
-        Li( Input( type = u"button", value = u"+", id = u"newNote", title = u"new note [ctrl-N]", class_ = u"button" ) ),
       ),
       id = u"toolbar",
       class_ = u"undisplayed", # start out as hidden, and then shown in the browser if the current notebook is read-write
