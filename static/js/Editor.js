57c456d83939cdb859390fcbd4a1b1b651ca09ef
Dan Helfman <dan@luminotes.com>
2007-09-08 00:12:42 +0000

    If you started a link and began typing, the link pulldown appeared when you type a space, even if the link isn't done.

--- static/js/Editor.js
+++ static/js/Editor.js
@@ -244,9 +244,9 @@ Editor.prototype.key_pressed = function ( event ) {
 Editor.prototype.key_released = function ( event ) {
   this.resize();
 
-  // if non-alphabetic (a-z), non-ctrl keys are released, issue a state changed event
+  // if non-alphabetic (a-z), non-space, non-ctrl keys are released, issue a state changed event
   var code = event.key().code;
-  if ( ( code >= 65 && code <= 90 ) || event.modifier().ctrl )
+  if ( ( code >= 65 && code <= 90 ) || code == 32 || event.modifier().ctrl )
     return;
 
   signal( this, "state_changed", this );
