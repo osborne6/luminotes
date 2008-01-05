0086c6366bb4574995c6bd9a8e56c096bd05ea7d
Dan Helfman <dan@luminotes.com>
2008-01-05 00:13:47 +0000

    Adding "sign up" link to demo account page and "upgrade" link to page of users with rate plan == 0.

--- view/User_area.py
+++ view/User_area.py
@@ -1,4 +1,4 @@
-from Tags import Div, H4, A
+from Tags import Div, Span, H4, A
 
 
 class User_area( Div ):
@@ -14,10 +14,27 @@ class User_area( Div ):
       ) or Div(
         u"logged in as %s" % ( user.username or u"a guest" ),
         " | ",
+        ( user.username == None ) and Span(
+          A(
+            u"sign up",
+            href = u"/sign_up",
+            title = u"Sign up for a real Luminotes account.",
+          ),
+          " | ",
+        ) or None,
+        ( user.username and user.rate_plan == 0 ) and Span(
+          A(
+            u"upgrade",
+            href = u"/upgrade",
+            title = u"Upgrade your Luminotes account.",
+          ),
+          " | ",
+        ) or None,
         A(
           u"logout",
           href = logout_url,
           id = u"logout_link",
+          title = u"Sign out of your account."
         ),
       ),
       id = u"user_area",
