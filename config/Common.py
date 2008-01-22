3d039cd14c8732a9c16cc22cc84fd6e0a32151b2
Dan Helfman <dan@luminotes.com>
2008-01-22 20:31:36 +0000

    Whoops.. Didn't mean to checkin modified Common.py config file. Reverting.

--- config/Common.py
+++ config/Common.py
@@ -23,7 +23,7 @@ settings = {
     "luminotes.http_proxy_ip": "127.0.0.1",
     "luminotes.https_proxy_ip": "127.0.0.2",
     "luminotes.support_email": "",
-    "luminotes.payment_email": "seller_1199677742_biz@luminotes.com",
+    "luminotes.payment_email": "",
     "luminotes.rate_plans": [
       {
         "name": "free",
@@ -38,30 +38,6 @@ settings = {
         "fee": 5,
         "button":
           """
-          <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top" class="subscribe_form">
-          <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/x-click-but24.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-          <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          <input type="hidden" name="cmd" value="_xclick-subscriptions">
-          <input type="hidden" name="business" value="seller_1199677742_biz@luminotes.com">
-          <input type="hidden" name="item_name" value="Luminotes Basic">
-          <input type="hidden" name="item_number" value="1">
-          <input type="hidden" name="no_shipping" value="1">
-          <input type="hidden" name="no_note" value="1">
-          <input type="hidden" name="currency_code" value="USD">
-          <input type="hidden" name="lc" value="US">
-          <input type="hidden" name="bn" value="PP-SubscriptionsBF">
-          <input type="hidden" name="a3" value="5.00">
-          <input type="hidden" name="p3" value="1">
-          <input type="hidden" name="t3" value="M">
-          <input type="hidden" name="src" value="1">
-          <input type="hidden" name="sra" value="1">
-          <input type="hidden" name="custom" value="%s">
-          <input type="hidden" name="modify" value="1">
-          <input type="hidden" name="return" value="http://luminotes.com:8083/users/thanks">
-          <input type="hidden" name="cancel_return" value="http://luminotes.com:8083/">
-          <input type="hidden" name="rm" value="2">
-          <input type="hidden" name="cbt" value="Return to Luminotes">
-          </form>
           """,
       },
       {
@@ -71,30 +47,6 @@ settings = {
         "fee": 9,
         "button":
           """
-          <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top" class="subscribe_form">
-          <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/x-click-but24.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-          <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          <input type="hidden" name="cmd" value="_xclick-subscriptions">
-          <input type="hidden" name="business" value="seller_1199677742_biz@luminotes.com">
-          <input type="hidden" name="item_name" value="Luminotes Standard">
-          <input type="hidden" name="item_number" value="2">
-          <input type="hidden" name="no_shipping" value="1">
-          <input type="hidden" name="no_note" value="1">
-          <input type="hidden" name="currency_code" value="USD">
-          <input type="hidden" name="lc" value="US">
-          <input type="hidden" name="bn" value="PP-SubscriptionsBF">
-          <input type="hidden" name="a3" value="9.00">
-          <input type="hidden" name="p3" value="1">
-          <input type="hidden" name="t3" value="M">
-          <input type="hidden" name="src" value="1">
-          <input type="hidden" name="sra" value="1">
-          <input type="hidden" name="custom" value="%s">
-          <input type="hidden" name="modify" value="1">
-          <input type="hidden" name="return" value="http://luminotes.com:8083/users/thanks">
-          <input type="hidden" name="cancel_return" value="http://luminotes.com:8083/">
-          <input type="hidden" name="rm" value="2">
-          <input type="hidden" name="cbt" value="Return to Luminotes">
-          </form>
           """,
       },
 #      {
@@ -109,9 +61,6 @@ settings = {
     ],
     "luminotes.unsubscribe_button":
       """
-      <a href="https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_subscr-find&alias=seller_1199677742_biz%40luminotes%2ecom" target="_top">
-      <img src="https://www.sandbox.paypal.com/en_US/i/btn/cancel_subscribe_gen_2_new.gif" border="0" alt="Unsubscribe">
-      </a>
       """,
   },
 }
