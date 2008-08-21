60aa794adc48b15f32a565c37b4ed32663d9e56a
Dan Helfman <dan@luminotes.com>
2008-08-21 13:44:51 -0700

    No longer disabling session filter on static files for desktop config.

--- config/Desktop.py
+++ config/Desktop.py
@@ -29,11 +29,9 @@ settings = {
   "/static": {
     "static_filter.on": True,
     "static_filter.dir": "static",
-    "session_filter.on": False,
   },
   "/favicon.ico": {
     "static_filter.on": True,
     "static_filter.file": "static/images/favicon.ico",
-    "session_filter.on": False,
   },
 }
