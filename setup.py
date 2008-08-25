6ba890c5b08b0e8469fd8ee6a0375dc7183aeb48
Dan Helfman <dan@luminotes.com>
2008-08-25 16:11:28 -0700

    Modified confirmation message to make clear that the user's data will be removed.

--- setup.py
+++ setup.py
@@ -115,8 +115,13 @@ class InnoScript:
           (self.name, path)
     print >> ofi
 
+    print >> ofi, r"[Messages]"
+    print >> ofi, r"ConfirmUninstall=Are you sure you want to completely remove %1, including all of your notebooks and notes?"
+    print >> ofi
+
     print >> ofi, r"[Run]"
     print >> ofi, r'Filename: "{app}\luminotes.exe"; Description: "Run Luminotes now"; Flags: nowait postinstall skipifsilent'
+    print >> ofi
 
     print >> ofi, r"[UninstallDelete]"
     print >> ofi, r'Type: files; Name: "{app}\luminotes.log"'
