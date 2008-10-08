122ef0cc2743285fb0e5b2e05ee7514e041f69ff
Dan Helfman <dan@luminotes.com>
2008-10-08 15:56:30 -0700

    Backing out extra indentation.

--- controller/Expire.py
+++ controller/Expire.py
@@ -6,11 +6,11 @@ def strongly_expire( function ):
   Decorator that sends headers that instruct browsers and proxies not to cache.
   """
   def expire( *args, **kwargs ):
-      cherrypy.response.headers[ "Expires" ] = "Sun, 19 Nov 1978 05:00:00 GMT"
-      cherrypy.response.headers[ "Cache-Control" ] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
-      cherrypy.response.headers[ "Pragma" ] = "no-cache"
+    cherrypy.response.headers[ "Expires" ] = "Sun, 19 Nov 1978 05:00:00 GMT"
+    cherrypy.response.headers[ "Cache-Control" ] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+    cherrypy.response.headers[ "Pragma" ] = "no-cache"
 
-      return function( *args, **kwargs )
+    return function( *args, **kwargs )
 
   return expire
 
@@ -22,8 +22,8 @@ def weakly_expire( function ):
   breaking Internet Explorer HTTPS file downloads.
   """
   def expire( *args, **kwargs ):
-      cherrypy.response.headers[ "Expires" ] = "Sun, 19 Nov 1978 05:00:00 GMT"
+    cherrypy.response.headers[ "Expires" ] = "Sun, 19 Nov 1978 05:00:00 GMT"
 
-      return function( *args, **kwargs )
+    return function( *args, **kwargs )
 
   return expire
